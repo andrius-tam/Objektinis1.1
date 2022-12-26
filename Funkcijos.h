d166f879f314499d455e687f9e8379663ad27e3b
andrius-tam <114215065+andrius-tam@users.noreply.github.com>
2022-12-26 22:45:35 +0200

    Add files via upload

--- Funkcijos.h
+++ Funkcijos.h
@@ -2,22 +2,72 @@
 #define STRUC_H_INCLUDED
 #include "mylib.h"
 
-struct duom {
+/*struct duom {
   string vardas;
   string pavarde;
   int paz_sk;
   vector <int> pazymiai;
   int egzaminas;
   double galutinis;
+};*/
+
+class Zmogus{
+protected:
+    string zmVardas;
+    string zmPavarde;
+public:
+    virtual void setVardas(string vardas) {zmVardas = vardas;}
+    virtual void setPavarde(string pavarde) {zmPavarde = pavarde;}
+    inline string getVardas() const {return zmVardas;}
+    inline string getPavarde() const {return zmPavarde;}
+    Zmogus(){
+        zmVardas = " ";
+        zmPavarde = " ";
+    }
+};
+
+class duom : public Zmogus{
+private:
+    string vardas;
+    string pavarde;
+    int paz_sk;
+    vector <int> pazymiai;
+    int egzaminas;
+    double galutinis;
+public:
+    ~duom(){pazymiai.clear();}
+    duom() {
+        egzaminas = 0;
+        galutinis = 0;
+    }
+    duom(const duom& naujas){
+        vardas = naujas.vardas;
+        pavarde = naujas.pavarde;
+        paz_sk = naujas.paz_sk;
+        egzaminas = naujas.egzaminas;
+        galutinis = naujas.galutinis;
+        pazymiai = naujas.pazymiai;
+    }
+    duom& operator=(const duom& naujas){
+        if(this == &naujas){return *this;}
+        vardas = naujas.vardas;
+        pavarde = naujas.pavarde;
+        paz_sk = naujas.paz_sk;
+        egzaminas = naujas.egzaminas;
+        galutinis = naujas.galutinis;
+        copy(naujas.pazymiai.begin(),naujas.pazymiai.end(),pazymiai.begin());
+        return *this;
+    }
+    double getBalas() const {return galutinis;}
+    void setPazymiai(vector <int> nd) {pazymiai = nd;}
+    inline int getEgzas() const {egzaminas;}
+    void setEgzas(int egzas) {egzaminas = egzas;}
+    void setPazKiek(int pazK) {paz_sk = pazK;}
+    void setBalas(int gal) {galutinis = gal;}
 };
 
 double vid_med (char tikrinimas, vector <int> &laik, int paz_sk, int egzaminas);
 void Nuskaitymas(vector <duom> &func, int &kiek, string pavadinimas, char tikrinimas);
-void Nuskaitymas(list <duom> &func, int &kiek, string pavadinimas, char tikrinimas);
-void Isvedimas(vector <duom> stud, int kiek, string pavadinimas_isv ,char tikrinimas);
-void Isvedimas(list <duom> stud, int kiek, string pavadinimas_isv ,char tikrinimas);
 void Generavimas(int dydis, string pavadinimas);
-void Skirstymas(vector <duom> &func, vector <duom> &vargsiukai, vector <duom> &galvociai);
-void Skirstymas(list <duom> &func, list <duom> &vargsiukai, list <duom> &galvociai);
-bool Palyginimas(duom pirmas, duom antras);
+bool Palyginimas(const duom &stud1, const duom &stud2);
 #endif // STRUC_H_INCLUDED
